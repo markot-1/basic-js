fd5876d603de8e9c2cd14a564eb1356e37c86392
Marina Kotova <m.klementyeva@mail.ru>
2023-04-14 14:18:45 +0200

    solve the following tasks: count cats, dream-team, get-email-domain

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let result = [];
+  if (typeof members === "object" && members != undefined) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] == "string") {
+        let name = members[i].trim();
+        result.push(name[0].toUpperCase());
+      }
+    }
+    return result.sort().join("");
+  }
+  return false;
 }
 
 module.exports = {
